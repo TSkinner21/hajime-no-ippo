03e40ea34fae865e52b18fea6be0fee9cdf6b46d
Joao Flores <joo_flore_21@hotmail.com>
2023-09-11 14:52:46 -0300

    feature: new fights

---
+++ src/components/lutas/lutas.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const luta1 = {
+    id: 1,
+    thumb: ippoVSsendo,
+    h1: "Makunouchi Ippo Vs Takeshi Sendou - LOLAPALOOZA",
+    description : "Makunouchi era no momento o desafiante número ao cinturão dos pesos leves do Japão. Sendou por sua vez, era o atual campeão. Com o cinturão em jogo, ambos realizam uma das lutas mais brutais do anime, ultrappassando a marca de 8 rounds. O apelido de LOLAPALOOZA é pelo sons que socos emitiam.",
+    first3 : true
+};
+
